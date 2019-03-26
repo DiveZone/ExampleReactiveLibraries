263f68e130739c57734882878fa2e9d401d02865
Willem-Jan Boogerd <willem-jan.boogerd@triopsys.nl>
2019-03-26 13:18:07 +0100

    Remove never finished ngrx-data example

--- reactive-demo-fe-ngrx-data/src/app/app.reducers.ts
+++
@@ -1,6 +0,0 @@
-import { ActionReducerMap } from '@ngrx/store';
-import { airlinesReducer } from './airlines/_store/airlines.reducer';
-
-export const reducers: ActionReducerMap<any> = {
-  airlines: airlinesReducer
-};
