a037eee7b6ad1b7b6d9884a2249d27b95e8b8d66
Willem-Jan Boogerd <wjboogerd@users.noreply.github.com>
2023-05-09 14:53:33 +0000

    ng16 migration ; remove @ngneat

--- reactive-demo-fe-ng/src/app/airlines/airlines-list/airlines-list.component.ts
+++ reactive-demo-fe-ng/src/app/airlines/airlines-list/airlines-list.component.ts
@@ -1,13 +1,11 @@
-import {AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import {take, takeUntil} from 'rxjs/operators';
-import {Subject} from 'rxjs';
-import {Airline} from '../airlines.model';
-import {AirlinesService} from '../airlines.service';
-import {UntilDestroy} from "@ngneat/until-destroy";
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { take } from 'rxjs/operators';
+import { Airline } from '../airlines.model';
+import { AirlinesService } from '../airlines.service';
 
-@UntilDestroy()
 @Component({
   selector: 'demo-airlines-list',
   templateUrl: './airlines-list.component.html'
@@ -20,7 +18,7 @@ export class AirlinesListComponent implements OnInit, AfterViewInit, OnChanges {
 
   @Input() country: string;
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(private _service: AirlinesService) {
   }
@@ -39,14 +37,15 @@ export class AirlinesListComponent implements OnInit, AfterViewInit, OnChanges {
 
   private updateList() {
     this._service.getAirlineList(this.country)
+      .pipe(takeUntilDestroyed())
       .subscribe({
-          next: (airlines) => {
-            this.dataSource.data = airlines;
-          },
-          error: () => {
-            this.dataSource.data = [];
-          }
+        next: (airlines) => {
+          this.dataSource.data = airlines;
+        },
+        error: () => {
+          this.dataSource.data = [];
         }
+      }
       );
   }
 
